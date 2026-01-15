8b3d3c133944a463f9d7191265e9e5df92e29f59
Jose Haroldo 1 <jsharoldo11998@gmail.com>
2026-01-15 00:43:49 +0000

    Eu não tenho certeza se é isso que você precisa, mas a logo da marca e í

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet" />
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/doselogotipo.png" />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
